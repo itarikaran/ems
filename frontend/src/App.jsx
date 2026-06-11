b3945d50b537ae88ef2413937f29e8855e518456
Ari Karan <arikaran4044@gmail.com>
2026-06-11 14:54:43 +0530

    delete frontend

--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import EmployeeList from "./components/EmployeeList";
-
-function App() {
-    return <EmployeeList />;
-}
-
-export default App;
--- frontend/src/services/EmployeeService.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/employees";
-
-export const getEmployees = () => {
-    return axios.get(API_URL);
-};
